ff26b185a954df414fb857e5ed58191856b3e644
Bronze14 <marcindadek@gmail.com>
2023-07-07 00:04:46 +0200

    fixing

--- app/components/Mainpage.tsx
+++ app/components/Mainpage.tsx
@@ -4,8 +4,8 @@ import { useBoardStore } from '@/store/BoardStore'
 import { useModalStore } from '@/store/ModalStore'
 import { Button } from '@material-tailwind/react'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid'
-
 import Link from 'next/link'
+
 export default function Mainpage() {
   const openModal = useModalStore((state) => state.openModal)
   const [board, getBoard, addPlus, addMinus] = useBoardStore((state) => [
@@ -14,34 +14,38 @@ export default function Mainpage() {
     state.addPlus,
     state.addMinus,
   ])
+
   useEffect(() => {
     getBoard()
   }, [getBoard])
+
   const handleAddTodo = () => {
     openModal()
   }
+
   if (!board || !Array.isArray(board.columns)) {
     return <p>No data available.</p>
   }
+
   return (
-    <header>
+    <div>
       <a>
         <Button
           variant="outlined"
-          className="flex items-center gap-4 p-6 ml-auto mt-10 mr-20 text-white-500 "
+          className="flex items-center gap-4 p-6 ml-auto mt-10 mr-20 text-white-500"
           onClick={handleAddTodo}
         >
           Add new Streamer
         </Button>
       </a>
       <div className="my-10 mx-20 relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full  text-sm text-left text-gray-500 dark:text-gray-400">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Streamer's name
               </th>
-              <th scope="col" className="px-6 py-3 ">
+              <th scope="col" className="px-6 py-3">
                 Points
               </th>
               <th scope="col" className="px-6 py-3">
@@ -55,44 +59,38 @@ export default function Mainpage() {
               </th>
             </tr>
           </thead>
-          {board.columns.map((column) => (
-            <tbody key={column.id}>
-              {column.streamers.map((streamer: Streamer) => (
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white  "
-                  >
+          <tbody>
+            {board.columns.map((column) =>
+              column.streamers.map((streamer) => (
+                <tr
+                  key={streamer.$id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     <Link
-                      key={streamer.$id}
                       href={`/${streamer.title}`}
                       className="hover:underline"
                     >
                       {streamer.title}
                     </Link>
-                  </th>
-
+                  </td>
                   <td className="px-6 py-4">{streamer.point}</td>
                   <td className="px-6 py-4">{streamer.description}</td>
                   <td className="px-6 py-4">{streamer.platform}</td>
                   <td className="px-6 py-4 text-right">
-                    <a className="font-medium text-white-600 dark:text-white-500 hover:text-blue-900 hover:underline">
-                      <button onClick={() => addPlus(streamer.$id)}>
-                        <ChevronUpIcon className="h-6 w-6 " />
-                      </button>
-                    </a>
-                    <a className="font-medium text-white-600 dark:text-white-500 hover:text-blue-900 hover:underline">
-                      <button onClick={() => addMinus(streamer.$id)}>
-                        <ChevronDownIcon className="h-6 w-6" />
-                      </button>
-                    </a>
+                    <button onClick={() => addPlus(streamer.$id)}>
+                      <ChevronUpIcon className="h-6 w-6 text-white-600 dark:text-white-500 hover:text-blue-900 hover:underline" />
+                    </button>
+                    <button onClick={() => addMinus(streamer.$id)}>
+                      <ChevronDownIcon className="h-6 w-6 text-white-600 dark:text-white-500 hover:text-blue-900 hover:underline" />
+                    </button>
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          ))}
+              )),
+            )}
+          </tbody>
         </table>
       </div>
-    </header>
+    </div>
   )
 }
